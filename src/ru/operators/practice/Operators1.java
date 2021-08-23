a4fd8fed43ac1a489a2ad5cd81560e8052439e86
Hamelmiher <gen4132@yandex.ru>
2021-08-24 01:26:50 +0500

    Operators1 finished

--- src/ru/operators/practice/Operators1.java
+++ src/ru/operators/practice/Operators1.java
@@ -2,6 +2,14 @@ package ru.operators.practice;
 
 public class Operators1 {
     public static void main(String[] args) {
-
+    double x = 10.0;
+    double y = 15.0;
+    double z = 7.0;
+    double avg = (x + y + z)/3;
+        System.out.println(avg);
+        avg = Math.floor(avg/= 2);
+            if (avg > 3){
+                System.out.println("Программа выполнена корректно");
+            }
     }
 }
