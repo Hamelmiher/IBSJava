0c25035a113538a593776aaa0402de9cadf133f8
Hamelmiher <gen4132@yandex.ru>
2021-08-24 01:53:49 +0500

    Operators1 fixed

--- src/ru/operators/practice/Operators1.java
+++ src/ru/operators/practice/Operators1.java
@@ -1,15 +1,33 @@
 package ru.operators.practice;
 
-public class Operators1 {
+import java.util.Scanner;
+
+class Operators1 {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-    double x = 10.0;
-    double y = 15.0;
-    double z = 7.0;
-    double avg = (x + y + z)/3;
-        System.out.println(avg);
-        avg = Math.floor(avg/= 2);
-            if (avg > 3){
-                System.out.println("Программа выполнена корректно");
-            }
+        double num1 = getInt();
+        double num2 = getInt();
+        double num3 = getInt();
+        double avg = (num1 + num2 + num3)/3;
+        System.out.println("Среднее арифметическое: "+ avg);
+        avg = Math.floor(avg / 2);
+        if (avg > 3){
+            System.out.println("Программа выполнена корректно");
+        }
     }
+
+    public static int getInt(){
+        System.out.println("Введите число:");
+        int num;
+        if(scanner.hasNextInt()){
+            num = scanner.nextInt();
+        } else {
+            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
+            scanner.next(); //рекурсия в случае ошибок
+            num = getInt();
+        }
+        return num;
+    }
+
 }
