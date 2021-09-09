aad11023804612a8926b933e04f1509b54cf4d97
Hamelmiher <gen4132@yandex.ru>
2021-09-10 00:29:36 +0500

    Final task 2 (findX) added

---
+++ src/ru/final2/findx/findX.java
@@ -0,0 +1,33 @@
+package ru.final2.findx;
+import java.util.Scanner;
+
+
+public class findX {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите уравнение: ");
+        String number1 = input.next();
+        String sign = input.next();
+        String number2 = input.next();
+        String equals = input.next();
+        String number3 = input.next();
+        if (sign.equals("-")) {
+            if (number1.equals("x")) {
+                System.out.println(Integer.parseInt(number3) + Integer.parseInt(number2));
+            }
+            if (number2.equals("x")) {
+                System.out.println(Integer.parseInt(number1) - Integer.parseInt(number3));
+            }
+            if (number3.equals("x")) {
+                System.out.println(Integer.parseInt(number1) - Integer.parseInt(number2));
+            }
+        } else {
+            if (number1.equals("x")) {
+                System.out.println(Integer.parseInt(number3) - Integer.parseInt(number2));
+            }
+            if (number2.equals("x")) {
+                System.out.println(Integer.parseInt(number3) - Integer.parseInt(number1));
+            }
+        }
+    }
+}
