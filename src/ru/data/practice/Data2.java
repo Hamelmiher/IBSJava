3fb0cd1f7b1582a80ba0a5e55ba68dc48cf6fdef
Hamelmiher <gen4132@yandex.ru>
2021-09-11 17:33:02 +0500

    Data2 added this is 12 practice work

---
+++ src/ru/data/practice/Data2.java
@@ -0,0 +1,29 @@
+package ru.data.practice;
+import java.util.Scanner;
+
+public class Data2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        input.useDelimiter("\r?\n"); //Я понимаю что это условия для работы сканера, но не понимаю как оно работает. Взял со stackowerflow
+        System.out.println("Введите \"I like Java!!!\"");
+        String javaLover = input.next();
+        if (javaLover.contains("Java")) {
+            if (javaLover.startsWith("I like")){
+                if (javaLover.endsWith("!!!")){
+                    System.out.println(javaLover.toUpperCase());
+                    String jovo = javaLover.substring(7,11);
+                    System.out.println(jovo.replaceAll("a", "o"));
+                }
+                else {
+                    System.out.println("Потерялись \"!!!\"");
+                }
+            }
+            else {
+                System.out.println("Потерялось \"I like\"");
+            }
+       }
+        else {
+            System.out.println("Потерялась \"Java\"");
+        }
+    }
+}
