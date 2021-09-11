6138d853466e201ce51d10f2db0fc35bf850d9f1
Hamelmiher <gen4132@yandex.ru>
2021-09-11 23:42:41 +0500

    Riddle refactored

--- src/ru/final4/riddle/Riddle.java
+++ src/ru/final4/riddle/Riddle.java
@@ -4,61 +4,69 @@ import java.util.Scanner;
 public class Riddle {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает");
+        String question = "Сидит дед, во сто шуб одет, кто его раздевает, тот слезы проливает";
+        String correctAnswer = "Заархивированный вирус";
+        String corrects = "Правильно!";
+        String help = "Подсказка";
+        String helpAnswer = "Проблемы в архиве";
+        String helpForbidden = "Подсказка недоступна";
+        String error = "Обидно, приходи в другой раз";
+        String nextAttempt = "Подумай!";
+        System.out.println(question);
         System.out.println("Попытка номер 1: ");
         String firstsTry = input.nextLine();
-        if (firstsTry.contains("Заархивированный вирус")) {
-            System.out.println("Правильно!");
+        if (firstsTry.contains(correctAnswer)) {
+            System.out.println(corrects);
             return;
         }
-        if (firstsTry.contains("Подсказка")) {
-            System.out.println("Проблемы в архиве");
-            String help = input.nextLine();
-            if (help.contains("Заархивированный вирус")) {
-                System.out.println("Правильно!");
+        if (firstsTry.contains(help)) {
+            System.out.println(helpAnswer);
+            String helpAsked = input.nextLine();
+            if (helpAsked.contains(correctAnswer)) {
+                System.out.println(corrects);
                 return;
             } else {
-                System.out.println("Обидно, приходи в другой раз");
+                System.out.println(error);
                 return;
             }
         } else {
-            System.out.println("Подумай!");
+            System.out.println(nextAttempt);
         }
         System.out.println("Попытка номер 2: ");
         String secondTry = input.nextLine();
-        if (secondTry.contains("Заархивированный вирус")) {
-            System.out.println("Правильно!");
+        if (secondTry.contains(correctAnswer)) {
+            System.out.println(corrects);
             return;
         }
-        if (secondTry.contains("Подсказка")) {
-            System.out.println("Подсказка недоступна");
+        if (secondTry.contains(help)) {
+            System.out.println(helpForbidden);
             String secondTryAfterHelp = input.nextLine();
-            if (secondTryAfterHelp.contains("Заархивированный вирус")) {
-                System.out.println("Правильно!");
+            if (secondTryAfterHelp.contains(correctAnswer)) {
+                System.out.println(corrects);
                 return;
             } else {
-                System.out.println("Обидно, приходи в другой раз");
+                System.out.println(error);
                 return;
             }
         } else {
-            System.out.println("Подумай!");
+            System.out.println(nextAttempt);
         }
         System.out.println("Попытка номер 3: ");
         String thirdTry = input.nextLine();
-        if (thirdTry.contains("Заархивированный вирус")) {
-            System.out.println("Правильно!");
+        if (thirdTry.contains(correctAnswer)) {
+            System.out.println(corrects);
             return;
         }
-        if (thirdTry.contains("Подсказка")) {
-            System.out.println("Подсказка недоступна");
+        if (thirdTry.contains(help)) {
+            System.out.println(helpForbidden);
             String thirdTryAfterHelp = input.nextLine();
-            if (thirdTryAfterHelp.contains("Заархивированный вирус")) {
-                System.out.println("Правильно!");
+            if (thirdTryAfterHelp.contains(correctAnswer)) {
+                System.out.println(corrects);
             } else {
-                System.out.println("Обидно, приходи в другой раз");
+                System.out.println(error);
             }
         } else {
-            System.out.println("Обидно, приходи в другой раз");
+            System.out.println(error);
         }
     }
 }
