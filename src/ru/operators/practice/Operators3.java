60a780754ac1d99898a37d4d5855645d52538ce0
Hamelmiher <gen4132@yandex.ru>
2021-12-13 12:50:34 +0500

    Operators 3 mass convertion fixed, fractional numbers can now be counted

--- src/ru/operators/practice/Operators3.java
+++ src/ru/operators/practice/Operators3.java
@@ -7,9 +7,10 @@ public class Operators3 {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int num = getInt();
-        int massOrDist = getMassOrDist();
-        int unit = 0;
+        System.out.println("Введите число: ");
+        double num = scanner.nextDouble();
+        double massOrDist = getMassOrDist();
+        double unit;
         if (massOrDist == 1) {
             unit = getUnitOfMass();
         } else {
@@ -18,19 +19,11 @@ public class Operators3 {
         calculate(massOrDist, unit, num);
     }
 
-    public static int getInt() {
-        System.out.println("Введите число: ");
-        while (!scanner.hasNextInt()) {
-            System.out.printf("Вы ввели : %s , введите число:", scanner.next());
-        }
-        return scanner.nextInt();
-    }
-
-    public static int getMassOrDist() {
+    public static double getMassOrDist() {
         boolean flag = true;
         int num = 0;
         while (flag) {
-            System.out.println("Введите число:  1 - масса, 2 - расстояние");
+            System.out.println("Выберите что переводить:  1 - масса, 2 - расстояние");
             if (!scanner.hasNextInt()) {
                 System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
                 scanner.next();
@@ -46,11 +39,11 @@ public class Operators3 {
         return num;
     }
 
-    public static int getUnitOfMeasure() {
+    public static double getUnitOfMeasure() {
         boolean flag = true;
-        int num = 0;
+        double num = 0;
         while (flag) {
-            System.out.println("Введите число:  1 - метр, 2 - миля, 3 - ярд, 4 - фут");
+            System.out.println("Выберите единицу измерения:  1 - метр, 2 - миля, 3 - ярд, 4 - фут");
             if (!scanner.hasNextInt()) {
                 System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
                 scanner.next();
@@ -66,9 +59,9 @@ public class Operators3 {
         return num;
     }
 
-    public static int getUnitOfMass() {
+    public static double getUnitOfMass() {
         boolean flag = true;
-        int num = 0;
+        double num = 0;
         while (flag) {
             System.out.println("Введите число:  1 - кг, 2 - г, 3 - мГ");
             if (!scanner.hasNextInt()) {
@@ -86,87 +79,71 @@ public class Operators3 {
         return num;
     }
 
-    public static void calculate(int massOrDist, int unit, int num) {
+    public static void calculate(double massOrDist, double unit, double num) {
         double result;
         double result2;
         double result3;
         double result4;
         if (massOrDist == 1) { // Выбрали массу
-            switch (unit) {
-                case 1: {
-                    result = num; //КГ
-                    result2 = num / 0.001; //Г
-                    result3 = num / 0.0001; //МГ
-                    System.out.println("Килограммы: " + result);
-                    System.out.println("Граммы: " + result2);
-                    System.out.println("Милиграммы: " + result3);
-                    break;
-                }
-                case 2: {
-                    result = num * 0.001;
-                    result2 = num;
-                    result3 = num / 0.0001;
-                    System.out.println("Килограммы: " + result);
-                    System.out.println("Граммы: " + result2);
-                    System.out.println("Милиграммы: " + result3);
-                    break;
-                }
-                case 3: {
-                    result = num * 0.001;
-                    result2 = num * 0.0001;
-                    result3 = num;
-                    System.out.println("Килограммы: " + result);
-                    System.out.println("Граммы: " + result2);
-                    System.out.println("Милиграммы: " + result3);
-                    break;
-                }
+            if (unit == 1) {
+                result = num; //КГ
+                result2 = num / 0.001; //Г
+                result3 = num / 0.000001; //МГ
+                System.out.println("Килограммы: " + result);
+                System.out.println("Граммы: " + result2);
+                System.out.println("Милиграммы: " + result3);
+            } else if (unit == 2) {
+                result = num * 0.001;
+                result2 = num;
+                result3 = num / 0.001;
+                System.out.println("Килограммы: " + result);
+                System.out.println("Граммы: " + result2);
+                System.out.println("Милиграммы: " + result3);
+            } else if (unit == 3) {
+                result = num * 0.000001;
+                result2 = num * 0.001;
+                result3 = num;
+                System.out.println("Килограммы: " + result);
+                System.out.println("Граммы: " + result2);
+                System.out.println("Милиграммы: " + result3);
             }
         } else { //Выбрали Расстояние
-            switch (unit) {
-                case 1: {
-                    result = num; //Метры
-                    result2 = num * 0.0006; //Мили
-                    result3 = num * 1.09361; //Ярды
-                    result4 = num * 3.28083; //Футы
-                    System.out.println("Метры: " + result);
-                    System.out.println("Мили: " + result2);
-                    System.out.println("Ярды: " + result3);
-                    System.out.println("Футы: " + result4);
-                    break;
-                }
-                case 2: {
-                    result = num * 1609.34;
-                    result2 = num;
-                    result3 = num * 1760;
-                    result4 = num * 5280;
-                    System.out.println("Метры: " + result);
-                    System.out.println("Мили: " + result2);
-                    System.out.println("Ярды: " + result3);
-                    System.out.println("Футы: " + result4);
-                    break;
-                }
-                case 3: {
-                    result = num * 0.9144;
-                    result2 = num * 0.0005;
-                    result3 = num;
-                    result4 = num * 3;
-                    System.out.println("Метры: " + result);
-                    System.out.println("Мили: " + result2);
-                    System.out.println("Ярды: " + result3);
-                    System.out.println("Футы: " + result4);
-                    break;
-                }
-                case 4: {
-                    result = num * 0.3048;
-                    result2 = num * 0.0001;
-                    result3 = num * 0.333;
-                    result4 = num;
-                    System.out.println("Метры: " + result);
-                    System.out.println("Мили: " + result2);
-                    System.out.println("Ярды: " + result3);
-                    System.out.println("Футы: " + result4);
-                    break;
-                }
+            if (unit == 1) {
+                result = num; //Метры
+                result2 = num * 0.0006; //Мили
+                result3 = num * 1.09361; //Ярды
+                result4 = num * 3.28083; //Футы
+                System.out.println("Метры: " + result);
+                System.out.println("Мили: " + result2);
+                System.out.println("Ярды: " + result3);
+                System.out.println("Футы: " + result4);
+            } else if (unit == 2) {
+                result = num * 1609.34;
+                result2 = num;
+                result3 = num * 1760;
+                result4 = num * 5280;
+                System.out.println("Метры: " + result);
+                System.out.println("Мили: " + result2);
+                System.out.println("Ярды: " + result3);
+                System.out.println("Футы: " + result4);
+            } else if (unit == 3) {
+                result = num * 0.9144;
+                result2 = num * 0.0005;
+                result3 = num;
+                result4 = num * 3;
+                System.out.println("Метры: " + result);
+                System.out.println("Мили: " + result2);
+                System.out.println("Ярды: " + result3);
+                System.out.println("Футы: " + result4);
+            } else if (unit == 4) {
+                result = num * 0.3048;
+                result2 = num * 0.0001;
+                result3 = num * 0.333;
+                result4 = num;
+                System.out.println("Метры: " + result);
+                System.out.println("Мили: " + result2);
+                System.out.println("Ярды: " + result3);
+                System.out.println("Футы: " + result4);
             }
         }
     }
